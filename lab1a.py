cf83098b5550af96dc0b90a6bbc01a449e82d57f
Dinh Vo <dvo12@myseneca.ca>
2026-05-15 12:49:04 -0400

    Added lab1a

---
+++ lab1a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Desciption: This program will output "hello word" to the screen.'''
+
+# Any line that start with # is als known as a comment,
+# these line are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line,
+# it is used to tell the system withc interpreter to
+# use(pythone3, python2, bash, etc).
+
+print('Hello world')
